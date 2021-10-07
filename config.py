554bab378c4b6c36334eaea4d4aa994022fdb28b
Skript_skript <33621981+navalnii@users.noreply.github.com>
2021-10-08 03:22:58 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-import sys 
+import sys
 
 
 def get_poopler_win32():
@@ -11,22 +11,22 @@ def get_poopler_win32():
             if 'bin' in directories:
                 poopler_dir = Path(path).joinpath('bin')
                 return poopler_dir
-                
+
     else:
         URL = 'https://blog.alivate.com.au/wp-content/uploads/2018/10/poppler-0.68.0_x86.7z'
-        headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
 
         filename = os.path.basename(URL)
         response = requests.get(URL,  headers=headers)
 
-
         if response.status_code == 200:
             with open(filename, 'wb') as out:
-                out.write(response.content)    
+                out.write(response.content)
 
             with py7zr.SevenZipFile(filename, mode='r') as z:
                 z.extractall('lib')
-                
+
             for path, directories, files in os.walk(str(Path.cwd())):
                 if 'bin' in directories:
                     poopler_dir = Path(path).joinpath('bin')
@@ -39,7 +39,7 @@ if sys.platform == 'win32':
     poopler_dir = get_poopler_win32()
     pytesseract_path = Path.cwd().joinpath('lib/Tesseract-OCR/tesseract.exe')
 
-    
+
 elif sys.platform == 'linux':
 
     from subprocess import STDOUT, check_call
@@ -47,16 +47,13 @@ elif sys.platform == 'linux':
                         libopencv-dev libtesseract-dev libleptonica-dev python-all-dev swig libcv-dev python-opencv \
                         python-numpy python-setuptools build-essential subversion tesseract-ocr-eng tesseract-ocr-dev\
                         libleptonica-dev python-all-dev swig libcv-dev'],
-                stdout=open(os.devnull,'wb'), stderr=STDOUT) 
-    
+               stdout=open(os.devnull, 'wb'), stderr=STDOUT)
+
     check_call(['apt-get', 'install', '-y', 'poppler-utils'],
-                stdout=open(os.devnull,'wb'), stderr=STDOUT) 
+               stdout=open(os.devnull, 'wb'), stderr=STDOUT)
 
 elif sys.platform == 'darwin':
 
     os.system('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2> /dev/null')
     os.system('brew install poppler')
     os.system('brew install tesseract')
-
-
-
